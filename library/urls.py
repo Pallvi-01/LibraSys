3709ffc02c27f5b5f88c308e45d7aab256d8663f
Pallvi <mepallvi@gmail.com>
2026-07-25 18:47:09 +0530

    fix login template

--- library/urls.py
+++ library/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('login/', views.user_login, name='login'),
+    path('books/', views.book_list, name='book_list'),
+]    
 
     # Book Management
 
